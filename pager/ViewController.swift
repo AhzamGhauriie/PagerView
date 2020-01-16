3d6d96dad28193435751629f3c85b92cad48fb38
Ahzam Ghauri <musharrafslash@gmail.com>
2020-01-16 20:30:16 +0500

    Initial Commit

---
+++ pager/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  pager
+//
+//  Created by HigherVisibility on 16/01/2020.
+//  Copyright © 2020 HigherVisibility. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ pagerTests/pagerTests.swift
@@ -0,0 +1,34 @@
+//
+//  pagerTests.swift
+//  pagerTests
+//
+//  Created by HigherVisibility on 16/01/2020.
+//  Copyright © 2020 HigherVisibility. All rights reserved.
+//
+
+import XCTest
+@testable import pager
+
+class pagerTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
