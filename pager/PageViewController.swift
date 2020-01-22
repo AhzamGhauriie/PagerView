4058d39c73908b4ae2f1afccdd3971ba0965574b
Ahzam Ghauri <musharrafslash@gmail.com>
2020-01-22 18:14:20 +0500

    first commit

---
+++ pager/PageViewController.swift
@@ -0,0 +1,20 @@
+//
+//  PageViewController.swift
+//  pager
+//
+//  Created by HigherVisibility on 19/01/2020.
+//  Copyright © 2020 HigherVisibility. All rights reserved.
+//
+
+import UIKit
+
+class PageViewController: UIPageViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+}
